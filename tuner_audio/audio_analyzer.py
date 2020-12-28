dfaa4170084ec44506dd07b72f96090a040d7e70
TomSchimansky <66446067+TomSchimansky@users.noreply.github.com>
2020-12-28 20:31:52 +0100

    Update audio_analyzer.py

--- tuner_audio/audio_analyzer.py
+++ tuner_audio/audio_analyzer.py
@@ -1,58 +1,72 @@
 from pyaudio import PyAudio, paInt16
+from threading import Thread
 import numpy as np
+import sys
 
 from tuner_settings.audio_settings import *
 
 
-def freq_to_number(f, a4_freq):
-    if f == 0:
-        print("Frequency is 0. Most likely your program has no acces to the microphone. Try to start the program from the command prompt, or give your ide acces to the mic.")
-        return 0
-    return 12 * np.log2(f / a4_freq) + 69
+class AudioAnalyzer(object):
+    def __init__(self, queue):
+        self.buffer = np.zeros(CHUNKSIZE * BUFFERTIMES)
+        self.running = False
 
-
-def number_to_freq(n, a4_freq):
-    return a4_freq * 2.0**((n - 69) / 12.0)
-
-
-def note_name(n):
-    return NOTE_NAMES[int(round(n) % 12)]
-
-
-def analyzing_function(runner, protectedList):
-    try:
-        p = PyAudio()
-        stream = p.open(format=paInt16, channels=1, rate=RATE, input=True, frames_per_buffer=CHUNKSIZE)
-    except Exception as e:
-        print(e)
-        return
-
-    # window = 0.5 * (1 - np.cos(np.linspace(0, 2 * np.pi, CHUNKSIZE * BUFFERTIMES, False))) # hanning-window (optional)
-
-    buffer = np.zeros(CHUNKSIZE * BUFFERTIMES)
-
-    while runner.running:
         try:
-            data = stream.read(CHUNKSIZE, exception_on_overflow=False)
+            self.audio_object = PyAudio()
+            self.stream = self.audio_object.open(format=paInt16,
+                                                 channels=1,
+                                                 rate=RATE,
+                                                 input=True,
+                                                 frames_per_buffer=CHUNKSIZE)
+        except Exception as e:
+            print('Error on line {}'.format(sys.exc_info()[-1].tb_lineno), type(e).__name__, e)
+            return
 
-            data = np.frombuffer(data, dtype=np.int16)
+        self.analyze_thread = Thread(target=self.analyzing_thread_function,
+                                     args=[queue])
 
-            buffer[:-CHUNKSIZE] = buffer[CHUNKSIZE:]
-            buffer[-CHUNKSIZE:] = data
+    def start(self):
+        self.running = True
+        self.analyze_thread.start()
 
-            numpydata = abs(np.fft.fft(buffer))
+    @staticmethod
+    def freq_to_number(f, a4_freq):
+        if f == 0:
+            print("""Frequency is 0. Most likely your program has no acces to the microphone.
+                     Try to start the program from the command prompt, or give your ide acces to the mic.""")
+            return 0
+        return 12 * np.log2(f / a4_freq) + 69
 
-            freqs = np.fft.fftfreq(len(numpydata), 1. / RATE)
+    @staticmethod
+    def number_to_freq(n, a4_freq):
+        return a4_freq * 2.0**((n - 69) / 12.0)
 
-            numpydata = numpydata[:int(len(numpydata) / 2)]
-            freqs = freqs[:int(len(freqs) / 2)]
+    @staticmethod
+    def note_name(n):
+        return NOTE_NAMES[int(round(n) % 12)]
 
-            protectedList.put(round(freqs[np.argmax(numpydata)], 2))
-        except:
-            print("missed samples")
-            pass
+    def analyzing_thread_function(self, queue):
+        """Main function where the microphone buffer gets read and
+           the fourier transformation gets applied"""
 
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
-    print("Audio Stream stopped")
+        while self.running:
+            try:
+                data = self.stream.read(CHUNKSIZE, exception_on_overflow=False)
+                data = np.frombuffer(data, dtype=np.int16)
+
+                self.buffer[:-CHUNKSIZE] = self.buffer[CHUNKSIZE:]
+                self.buffer[-CHUNKSIZE:] = data
+
+                numpydata = abs(np.fft.fft(self.buffer))
+                numpydata = numpydata[:int(len(numpydata) / 2)]
+
+                freqs = np.fft.fftfreq(len(numpydata), 1. / RATE)
+
+                queue.put(round(freqs[np.argmax(numpydata)], 2))
+
+            except Exception as e:
+                print('Error on line {}'.format(sys.exc_info()[-1].tb_lineno), type(e).__name__, e)
+
+        self.stream.stop_stream()
+        self.stream.close()
+        self.audio_object.terminate()
