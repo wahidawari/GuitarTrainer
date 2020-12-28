fecd7fdc658f978398bd2d67b94f2993f1b23069
TomSchimansky <66446067+TomSchimansky@users.noreply.github.com>
2020-12-28 20:33:01 +0100

    Update and rename global_settings.py to app_settings.py

---
+++ tuner_settings/app_settings.py
@@ -0,0 +1,9 @@
+
+APP_NAME = "GuitarTuner"
+ABOUT_TEXT = "made by Tom Schimansky 2020"
+
+WIDTH = 450  # window size when starting the app
+HEIGHT = 440
+
+FPS = 60
+CANVAS_SIZE = 280  # size of the audio-display
--- tuner_settings/global_settings.py
+++
@@ -1,8 +0,0 @@
-
-APP_NAME = "GuitarTuner"
-ABOUT_TEXT = "made by Tom Schimansky 2020"
-
-WIDTH = 450
-HEIGHT = 440
-FPS = 60
-CANVAS_SIZE = 280
