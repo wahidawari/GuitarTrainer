dda045a414f328b236e6e561d0fb95cd4a5faecf
TomSchimansky <66446067+TomSchimansky@users.noreply.github.com>
2020-12-28 20:27:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,18 @@ import os
 import sys
 
 from numpy import zeros, average
-from threading import Thread
 import time
 
-from tuner_audio.audio_analyzer import analyzing_function, freq_to_number, number_to_freq, note_name
-from tuner_audio.threading_helper import ProtectedList, Runner, GlobalManager, SoundThread
+from tuner_audio.audio_analyzer import AudioAnalyzer
+from tuner_audio.threading_helper import ProtectedList, GlobalManager
+from tuner_audio.sound_thread import SoundThread
 
 from tuner_appearance.color_manager import ColorManager
 from tuner_appearance.image_manager import ImageManager
 from tuner_appearance.tuner_ui_parts.main_frame import MainFrame
 from tuner_appearance.tuner_ui_parts.settings_frame import SettingsFrame
 
-from tuner_settings.global_settings import *
+from tuner_settings.app_settings import *
 
 MAIN_PATH = os.path.dirname(__file__)
 
@@ -26,25 +26,22 @@ class App(tkinter.Tk):
 
         self.color_manager = ColorManager()
         self.image_manager = ImageManager(MAIN_PATH)
-        self.runner = Runner()
         self.global_manager = GlobalManager()
-        self.queue = ProtectedList()
+        self.frequency_queue = ProtectedList()
 
         self.main_frame = MainFrame(self)
         self.settings_frame = SettingsFrame(self)
 
-        self.analyzeThread = Thread(target=analyzing_function,
-                                    args=[self.runner, self.queue])
-        self.analyzeThread.start()
+        self.audio_analyzer = AudioAnalyzer(self.frequency_queue)
+        self.audio_analyzer.start()
 
-        self.playSoundThread = SoundThread(self.runner,
-                                           MAIN_PATH + "/assets/sounds/drop.wav",
-                                           self.global_manager,
-                                           self.main_frame.buttonBell)
-        self.playSoundThread.start()
+        self.play_sound_thread = SoundThread(MAIN_PATH + "/assets/sounds/drop.wav",
+                                             self.global_manager,
+                                             self.main_frame.mute_button)
+        self.play_sound_thread.start()
 
-        self.needleBuffer = zeros(5)
-        self.hitCounter = 0
+        self.needle_buffer_array = zeros(5)
+        self.tone_hit_counter = 0
 
         self.minsize(WIDTH, HEIGHT)
         self.resizable(True, True)
@@ -81,51 +78,51 @@ class App(tkinter.Tk):
         self.main_frame.place(relx=0, rely=0, relheight=1, relwidth=1)
 
     def on_closing(self, event=0):
-        self.runner.running = False
+        self.audio_analyzer.running = False
+        self.play_sound_thread.running = False
         self.destroy()
-        exit()
 
     def start(self):
-        while self.runner.running:
+        while self.audio_analyzer.running:
             try:
-                freq = self.queue.get()
+                freq = self.frequency_queue.get()
                 if freq is not None:
 
-                    number = freq_to_number(freq, self.global_manager.a4_freq)
-                    note = note_name(number)
-                    difference = number_to_freq(round(number), self.global_manager.a4_freq) - freq
-                    differenceNextNote = number_to_freq(round(number), self.global_manager.a4_freq) - \
-                                         number_to_freq(round(number - 1), self.global_manager.a4_freq)
+                    number = self.audio_analyzer.freq_to_number(freq, self.global_manager.a4_freq)
+                    note = self.audio_analyzer.note_name(number)
+                    difference = self.audio_analyzer.number_to_freq(round(number), self.global_manager.a4_freq) - freq
+                    difference_next_note = self.audio_analyzer.number_to_freq(round(number), self.global_manager.a4_freq) - \
+                                         self.audio_analyzer.number_to_freq(round(number - 1), self.global_manager.a4_freq)
 
-                    needleAngle = -90 * (difference / differenceNextNote * 2)
+                    neddle_angle = -90 * (difference / difference_next_note * 2)
 
-                    if abs(needleAngle) < 5:
+                    if abs(neddle_angle) < 5:
                         self.main_frame.underCanvas.itemconfig(self.main_frame.needle, fill=self.color_manager.green)
-                        self.hitCounter += 1
+                        self.tone_hit_counter += 1
                     else:
                         self.main_frame.underCanvas.itemconfig(self.main_frame.needle, fill=self.color_manager.red)
-                        self.hitCounter = 0
+                        self.tone_hit_counter = 0
 
-                    if self.hitCounter > 7:
-                        self.hitCounter = 0
-                        self.global_manager.playSound = True
+                    if self.tone_hit_counter > 7:
+                        self.tone_hit_counter = 0
+                        self.global_manager.play_sound = True
 
-                    self.needleBuffer[:-1] = self.needleBuffer[1:]
-                    self.needleBuffer[-1:] = needleAngle
+                    self.needle_buffer_array[:-1] = self.needle_buffer_array[1:]
+                    self.needle_buffer_array[-1:] = neddle_angle
 
-                    self.main_frame.set_needle_angle(average(self.needleBuffer))
+                    self.main_frame.set_needle_angle(average(self.needle_buffer_array))
 
                     self.main_frame.noteLabel.configure(text=note)
                     self.main_frame.buttonHertz.label.configure(text=str(round(-difference, 1)) + " Hz")
 
                 self.update()
+
             except Exception as e:
                 print('Error on line {}'.format(sys.exc_info()[-1].tb_lineno), type(e).__name__, e)
                 time.sleep(1 / FPS)
-
-        print("-- program ended --")
 
 
 if __name__ == "__main__":
     app = App()
     app.start()
+
