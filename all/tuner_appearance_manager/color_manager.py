a18d46edb276735ee625343ab5d4ffe9f6eddf0a
wahidawari <71400900+wahidawari@users.noreply.github.com>
2021-03-10 00:54:49 -0800

    Update color_manager.py

--- all/tuner_appearance_manager/color_manager.py
+++ all/tuner_appearance_manager/color_manager.py
@@ -17,27 +17,17 @@ class ColorManager(object):
         self.set_mode("Light")
 
     def set_mode(self, mode):
-        if mode == "Dark":
-            self.background_layer_1 = self.rgb_to_hex((50, 50, 50))
-            self.background_layer_0 = self.rgb_to_hex((33, 33, 33))
-            self.text_main = self.rgb_to_hex((255, 255, 255))
-            self.text_2 = self.rgb_to_hex((169, 169, 169))
-            self.theme_main = self.rgb_to_hex((51, 94, 145))
-            self.theme_dark = self.rgb_to_hex((26, 51, 82))
-            self.theme_light = self.rgb_to_hex((85, 140, 200))
-            self.needle = self.rgb_to_hex((107, 42, 28))
-            self.needle_hit = self.rgb_to_hex((43, 113, 53))
+        self.background_layer_1 = self.rgb_to_hex((50, 50, 50))
+        self.background_layer_0 = self.rgb_to_hex((33, 33, 33))
+        self.text_main = self.rgb_to_hex((255, 255, 255))
+        self.text_2 = self.rgb_to_hex((169, 169, 169))
+        self.theme_main = self.rgb_to_hex((51, 94, 145))
+        self.theme_dark = self.rgb_to_hex((26, 51, 82))
+        self.theme_light = self.rgb_to_hex((85, 140, 200))
+        self.needle = self.rgb_to_hex((107, 42, 28))
+        self.needle_hit = self.rgb_to_hex((43, 113, 53))
 
-        elif mode == "Light":
-            self.background_layer_1 = self.rgb_to_hex((241, 239, 238))
-            self.background_layer_0 = self.rgb_to_hex((209, 208, 206))
-            self.text_main = self.rgb_to_hex((0, 0, 0))
-            self.text_2 = self.rgb_to_hex((44, 44, 44))
-            self.theme_main = self.rgb_to_hex((83, 147, 213))
-            self.theme_dark = self.rgb_to_hex((51, 94, 145))
-            self.theme_light = self.rgb_to_hex((128, 175, 223))
-            self.needle = self.rgb_to_hex((107, 42, 28))
-            self.needle_hit = self.rgb_to_hex((43, 113, 53))
+      
 
     @staticmethod
     def rgb_to_hex(rgb):
